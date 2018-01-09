4bd9b93f95e58ccd86071db676b413a77dda5001
jennyliindgren <jenny.lindgren@hotmail.com>
2018-01-09 19:45:29 +0100

    flags in kitchen

--- public/js/ingredients.js
+++ public/js/ingredients.js
@@ -36,10 +36,6 @@ Vue.component('ingredient-limited', {
    </div>',         
 });
 
-
-
-
-
 var vm = new Vue({
   el: '#ingredients',
   mixins: [sharedVueStuff],
@@ -55,12 +51,10 @@ var vm = new Vue({
     },
      refreshPage: function(){
         window.location = 'http://localhost:3000/ingredients';
-
     },
     ShowStartpage: function(){
         window.location = 'http://localhost:3000/kitchen';
         document.getElementById("finishedOrder").style.display ="none";
-        
     },
     filteredIngredients: function () {
         var resultList = [];
@@ -68,12 +62,8 @@ var vm = new Vue({
             if(this.ingredients[i]["ingredient_" +this.lang].substring(0,this.searchText.length) == this.searchText) {
                 resultList.push(this.ingredients[i]);
             }
-            
         }
         return resultList;
     },
-    
-   
-
 
 }});
