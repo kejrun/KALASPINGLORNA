bbe9ca33a8119d3386604d6c1e0e9025a95cee1f
kejrun <kasv5091@gmail.com>
2018-01-10 11:37:22 +0100

    auto indent

--- public/js/kitchen.js
+++ public/js/kitchen.js
@@ -5,36 +5,35 @@
 Vue.component('order-item', {
   props: ['uiLabels', 'order', 'orderId', 'lang'],
   template: '<div id = style-orderbutton><div class = orderInfo>{{orderId}} <div v-for="o in order.order"><h2>{{o["type"]}}</h2>\
-    </div></div><div v-for="o in order.order"><h6>{{o["name"]}}</h6><div v-for="ing in o.ingredients" class = orderIngredInfo>{{ ing["ingredient_" + lang] }}</div></div></div>'
+  </div></div><div v-for="o in order.order"><h6>{{o["name"]}}</h6><div v-for="ing in o.ingredients" class = orderIngredInfo>{{ ing["ingredient_" + lang] }}</div></div></div>'
 });
 
 Vue.component('order-item-to-prepare', {
   props: ['uiLabels', 'order', 'orderId', 'lang'],
   template: '<div class = style_orders_inQueue>\
-           <button class = ArrowImg_dis>\
-            <img src="https://abcdefghijklmn-pqrstuvwxyz.com/wp-content/themes/o/img/prev.svg" width="30">\
-            <br>{{uiLabels.cancel}}\
-          </button>\
-            <button v-on:click="orderInMade" id="Drink_items">\
-          <order-item\
-            :ui-labels="uiLabels"\
-            :lang="lang"\
-            :order-id="orderId"\
-            :order="order">\
-          </order-item>\
-            </button>\
-            <button class = ArrowImg_dis>\
-            <img src="https://abcdefghijklmn-pqrstuvwxyz.com/wp-content/themes/o/img/next.svg" width="30">\
-            <br>{{uiLabels.ready}}\
-          </button>\
-         </div>',
-    methods: {
+  <button class = ArrowImg_dis>\
+  <img src="https://abcdefghijklmn-pqrstuvwxyz.com/wp-content/themes/o/img/prev.svg" width="30">\
+  <br>{{uiLabels.cancel}}\
+  </button>\
+  <button v-on:click="orderInMade" id="Drink_items">\
+  <order-item\
+  :ui-labels="uiLabels"\
+  :lang="lang"\
+  :order-id="orderId"\
+  :order="order">\
+  </order-item>\
+  </button>\
+  <button class = ArrowImg_dis>\
+  <img src="https://abcdefghijklmn-pqrstuvwxyz.com/wp-content/themes/o/img/next.svg" width="30">\
+  <br>{{uiLabels.ready}}\
+  </button>\
+  </div>',
+  methods: {
     orderInMade: function(){
-        this.$emit('in-made');
+      this.$emit('in-made');
     }
-    }
-  });
-
+  }
+});
 
 var vm = new Vue({
   el: '#orders',
@@ -46,27 +45,27 @@ var vm = new Vue({
     sendCancel: function (orderid) {
       socket.emit("cancelOrder", orderid);
     },
-    markInMade: function(orderid){ 
+    markInMade: function(orderid){
       socket.emit("orderInMade", orderid);
     },
     unmarkInMade: function(orderid){
       socket.emit("notInMade", orderid);
     },
     undoCancel: function(orderid){
-        socket.emit("undoCancelOrder", orderid);
+      socket.emit("undoCancelOrder", orderid);
     },
     wantToCancel: function (orderid){
-        socket.emit("wantCancel", orderid); 
-    },                     
+      socket.emit("wantCancel", orderid);
+    },
     ShowHistory: function(){
       document.getElementById("finishedOrder").style.display ="block";
       document.getElementById("start_page").style.display = "none";
       document.getElementById("Hist_Ingred").style.display = "none";
     },
     ShowStartpage: function(){
-        document.getElementById("finishedOrder").style.display ="none";
-        document.getElementById("start_page").style.display = "block";
-        document.getElementById("Hist_Ingred").style.display = "block"; 
+      document.getElementById("finishedOrder").style.display ="none";
+      document.getElementById("start_page").style.display = "block";
+      document.getElementById("Hist_Ingred").style.display = "block";
     }
   }
 });
@@ -74,26 +73,26 @@ var vm = new Vue({
 Vue.component('order-item-to-prepare-in-made', {
   props: ['uiLabels', 'order', 'orderId', 'lang'],
   template: '<div class = style_orders_inQueue>\
-            <button v-on:click="wantCancel" class = ArrowImg>\
-             <img src="https://abcdefghijklmn-pqrstuvwxyz.com/wp-content/themes/o/img/prev.svg" width="30">\
-            <br>{{uiLabels.cancel}}\
-          </button>\
-            <button v-on:click="notInMade" id="inMade_Drink_items">\
-            <order-item\
-            :ui-labels="uiLabels"\
-            :lang="lang"\
-            :order-id="orderId"\
-            :order="order">\
-          </order-item>\
-            </button>\
-            <button v-on:click="orderDone" class = ArrowImg>\
-            <img src="https://abcdefghijklmn-pqrstuvwxyz.com/wp-content/themes/o/img/next.svg" width="30">\
-            <br>{{uiLabels.ready}}\
-          </button>\
-         </div>',
-    methods: {
+  <button v-on:click="wantCancel" class = ArrowImg>\
+  <img src="https://abcdefghijklmn-pqrstuvwxyz.com/wp-content/themes/o/img/prev.svg" width="30">\
+  <br>{{uiLabels.cancel}}\
+  </button>\
+  <button v-on:click="notInMade" id="inMade_Drink_items">\
+  <order-item\
+  :ui-labels="uiLabels"\
+  :lang="lang"\
+  :order-id="orderId"\
+  :order="order">\
+  </order-item>\
+  </button>\
+  <button v-on:click="orderDone" class = ArrowImg>\
+  <img src="https://abcdefghijklmn-pqrstuvwxyz.com/wp-content/themes/o/img/next.svg" width="30">\
+  <br>{{uiLabels.ready}}\
+  </button>\
+  </div>',
+  methods: {
     orderDone: function () {
-      this.$emit('done');   
+      this.$emit('done');
     },
     notInMade: function(){
       this.$emit('in-made');
@@ -102,23 +101,22 @@ Vue.component('order-item-to-prepare-in-made', {
       this.$emit('want-cancel')
     }
   }
-
 });
 
 Vue.component('order-cancel', {
-    props: ['uiLabels', 'order', 'orderId', 'lang'],
-    template: '<div class = cancelOrNot><div class = cancelOrNotOrder >{{uiLabels.CancelWant}}<div v-for="o in order.order" class = orderIngredCancel>{{orderId}}, {{o["type"]}}<div v-for="ing in o.ingredients" class = orderIngredInfo>{{ ing["ingredient_" + lang] }}</div></div></div>\
-    <button v-on:click="cancelOrder" class = YesNoButton>{{uiLabels.yes}}</button>\
-    <button v-on:click="undoCancelOrder" class = YesNoButton>{{uiLabels.no}}</button></div>',
-    methods: {
-      cancelOrder: function () {
-        this.$emit('cancel')
+  props: ['uiLabels', 'order', 'orderId', 'lang'],
+  template: '<div class = cancelOrNot><div class = cancelOrNotOrder >{{uiLabels.CancelWant}}<div v-for="o in order.order" class = orderIngredCancel>{{orderId}}, {{o["type"]}}<div v-for="ing in o.ingredients" class = orderIngredInfo>{{ ing["ingredient_" + lang] }}</div></div></div>\
+  <button v-on:click="cancelOrder" class = YesNoButton>{{uiLabels.yes}}</button>\
+  <button v-on:click="undoCancelOrder" class = YesNoButton>{{uiLabels.no}}</button></div>',
+  methods: {
+    cancelOrder: function () {
+      this.$emit('cancel')
     },
     undoCancelOrder:function(){
-        this.$emit('undo-cancel')
+      this.$emit('undo-cancel')
     }
-    }
-}); 
+  }
+});
 
 Vue.component('order-item-done', {
   props: ['uiLabels', 'order', 'orderId', 'lang'],
@@ -126,52 +124,49 @@ Vue.component('order-item-done', {
 });
 
 function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock_kitchen').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock_kitchen').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
 };
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+  if (i < 10) {i = "0" + i};
+  return i;
 };
 
 //Stocklist page
 Vue.component('ingredient', {
   props: ['item', 'lang'],
   template: '<div class="ingredientStock">\
-    <div class= "itemColumn">{{item["ingredient_"+ lang]}}</div>\
-    <div class= "itemColumn">{{item.stock}}ml</div>\
-    <div class= "itemColumn"><button v-on:click="minusIngredient" class="MinusPlusButtons" name="ingredientsMinusButton">-</button>\
+  <div class= "itemColumn">{{item["ingredient_"+ lang]}}</div>\
+  <div class= "itemColumn">{{item.stock}}ml</div>\
+  <div class= "itemColumn"><button v-on:click="minusIngredient" class="MinusPlusButtons" name="ingredientsMinusButton">-</button>\
   <label id="counterStock">{{ counter }}</label>\
   <button v-on:click="plusIngredient" class="MinusPlusButtons" name="ingredientsPlusButton">+</button>\</div>\
-   </div>',         
+  </div>',
   data: function () {
     return {
       counter: 0
     };
   },
   methods: {
-     plusIngredient: function(){
-        this.$emit('refill');
-        this.counter += 1000;
-        console.log(this)
+    plusIngredient: function(){
+      this.$emit('refill');
+      this.counter += 1000;
+      console.log(this)
     },
-     minusIngredient: function(){
-        this.$emit('un-refill');
-        this.counter -= 1000;
-     },
+    minusIngredient: function(){
+      this.$emit('un-refill');
+      this.counter -= 1000;
+    },
     resetCounter: function () {
       this.counter = 0;
     }
-    }
+  }
 });
-
-
-
